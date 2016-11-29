9639c2eed649f213f9f0ce04cf7f3e2aba969ab9
Ubuntu <ubuntu@csvm>
2016-11-29 09:18:26 -0500

    image updater

--- GUI (Documentation) .mli
+++ GUI (Documentation) .mli
@@ -1,16 +1,12 @@
+
 (* A [gui] regulates the positions of the buttons for the gui
  * The state can be used by other parts to set up graphics and interface. *)
-module type GUI = sig
+module type Gui = sig
 
-(* [click] stores the location of mouse in coordinates (x,y) *)
-type click = (int * int)
-
-(* The type of camera view shift, either left, right, up, or down. *)
-type dir
-
+val create_disp : unit -> Sdlvideo.surface
 (* [update_disp loc] returns the state after determining what, if any,
  * button was clicked by mapping the click location to the buttons on the gui.
  * Updates state if necessary. *)
-val update_disp : click -> unit
+val update_disp : string -> string -> Sdlvideo.surface -> unit
 
 end
--- GUI.ml
+++ GUI.ml
@@ -1,10 +1,19 @@
-module GUI = struct
+open Sdl
+open Sdlloader
 
-type click = int*int
-type dir = | Left | Right | Up | Down
+module Gui = struct
 
-let update_disp click =
+  let create_disp () = let x = Sdlvideo.set_video_mode 600 450 [`DOUBLEBUF] in
+  Sdlvideo.unset_clip_rect x; x
+
+let update_disp previous_image new_image screen =
   (* process click and call state? *)
-  failwith "unimplmented"
+  if previous_image = new_image then ()
+else
+  let image = Sdlloader.load_image ("Rooms/" ^ new_image) in
+  let position_of_image = Sdlvideo.rect 0 0 0 0 in
+  Sdlvideo.blit_surface ~dst_rect:position_of_image ~src:image ~dst:screen ();
+  Sdlvideo.flip screen
+(* ocamlbuild -use-ocamlfind -tag thread,sdl,sdl.sdlimage -pkgs oUnit,yojson,str,ANSITerminal,async,threads,sdl,sdl.sdlimage GUI.byte   *)
 
 end
