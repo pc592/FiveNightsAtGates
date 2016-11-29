6a56f94b6cf6b5f0d6d24fdd0d9acc38ab84c2a7
Pui Lam <pc592@cornell.edu>
2016-11-29 13:53:19 -0500

    map overlay

--- GUI.ml
+++ GUI.ml
@@ -15,8 +15,11 @@ else
   let font = Sdlttf.open_font "Timea.ttf" 14 in
   let text = Sdlttf.render_test_blended font (print_time st) ~fg:Sdlvideo.white in
   let position_of_text = Sdlvideo.rect 150 0 150 150 in
+  let map = Sdlloader.load_image ("map.png") in
+  let position_of_map = Sdlvideo.rect 0 0 0 0 in
   Sdlvideo.blit_surface ~dst_rect:position_of_image ~src:image ~dst:screen ();
-  Sdlvideo.blit_surface ~dst_rect:position_of_text ~src:text ~dst:screen();
+  Sdlvideo.blit_surface ~dst_rect:position_of_text ~src:text ~dst:screen ();
+  Sdlvideo.blit_surface ~dst_rect:position_of_map ~src:map ~dst:screen ();
   Sdlvideo.flip screen
 (* ocamlbuild -use-ocamlfind -tag thread,sdl,sdl.sdlimage -pkgs oUnit,yojson,str,ANSITerminal,async,threads,sdl,sdl.sdlimage GUI.byte   *)
 
