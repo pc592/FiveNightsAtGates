1e3db9c21d9bab79a4056e6323b9d2567d2fccdd
Pui Lam <pc592@cornell.edu>
2016-11-29 12:42:34 -0500

    possible keyboard input code in test.ml

--- test.ml
+++ test.ml
@@ -1,8 +1,21 @@
-let font_filename = "Timea.ttf"
 
-...
-let font = Sdlttf.open_font font_filename 14 in
-let text = Sdlttf.render_test_blended font (print_time st) ~fg:Sdlvideo.white in
-let position_of_text = Sdlvideo.rect 300 0 300 300 in
-...
-Sdlvideo.blit_surface ~dst_rect:position_of_text ~src:text ~dst:screen()
+
+(* getKey () functions like read_line ().
+ * Return the string equivalent so that process_cmd is still the same.*)
+let cmd =
+  let rec getKey () =
+    match wait_event () with
+    | KEYDOWN {keysym=KEY_UP} -> "up"
+    | KEYDOWN {keysym=KEY_DOWN} -> "down"
+    | KEYDOWN {keysym=KEY_RIGHT} -> "right"
+    | KEYDOWN {keysym=KEY_LEFT} -> "left"
+    | KEYDOWN {keysym=KEY_SPACE} -> "next"
+    | KEYDOWN {keysym=KEY_ESCAPE} -> "quit"
+    | KEYDOWN {keysym=KEY_a} -> "close one"
+    | KEYDOWN {keysym=KEY_s} -> "close_two"
+    | KEYDOWN {keysym=KEY_d} -> "open one"
+    | KEYDOWN {keysym=KEY_f} -> "open two"
+    | KEYDOWN {keysym=Key_w} -> "main"
+    | KEYDOWN {keysym=Key_e} -> "camera"
+    | event -> print_endline (string_of_event event) ^ " is not a valid command";
+               getKey ()
